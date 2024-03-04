beaa4ad67b37ab6e787ed9f5d52b095183751a41
suranol <aira.suranol02@gmail.com>
2024-03-05 00:03:15 +0800

    Added PageNotAnInteger error handling

--- thesis_app/views.py
+++ thesis_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 
@@ -11,6 +11,8 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     # __import__('pdb').set_trace()
