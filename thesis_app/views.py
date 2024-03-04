9ede776902bc96a064a55d81bea5e127de0ba667
suranol <aira.suranol02@gmail.com>
2024-03-05 01:17:30 +0800

    Added comment feature and its form

--- thesis_app/views.py
+++ thesis_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
+from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -21,5 +23,19 @@ def post_list(request):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
 
-    return render(request, "posts/detail.html", {"post":post})
+    return render(request, "posts/detail.html", {"post":post,"comments":comments, "form": form})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form .save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request, "comment.html", {'post': post, 'form': form, 'comment': comment})
