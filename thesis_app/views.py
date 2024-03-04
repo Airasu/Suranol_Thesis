dac8f394644abe6fe34a064c308b24832edb2371
suranol <aira.suranol02@gmail.com>
2024-03-05 00:00:21 +0800

    Added paginator empty page

--- thesis_app/views.py
+++ thesis_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 
 
@@ -9,14 +9,15 @@ def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     # __import__('pdb').set_trace()
     return render(request, "posts/list.html", {"posts":posts})
-
-
     
-def post_detail(request, year, month, day, post_slug):
-    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post_slug, publish__year=year, publish__month=month, publish__day=day)
 
-    return render(request, "posts/detail.html", {"post": post})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
 
+    return render(request, "posts/detail.html", {"post":post})
